f6c2a4f0e3b98f2aeba10746b815c35c680c70a3
quanph14 <phquan.0493@gmail.com>
2022-11-23 22:56:43 +0700

    done 2311

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-playlist-app';
+  title = 'angular-product-management';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YoutubePlayerComponent,
+    YoutubePlaylistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterLink,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
